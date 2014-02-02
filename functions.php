b9ebbfb870fa67a7dd00ec2b8e7eeccafb65af17
Erin <hoople.erin@gmail.com>
2014-02-02 12:20:21 -0600

    delete favicon, change excerpt length

--- functions.php
+++ functions.php
@@ -1,9 +1,4 @@
-// add a favicon to your
-2
-function blog_favicon() {
-3
-echo '<link rel="Shortcut Icon" type="image/x-icon" href="'.get_bloginfo('wpurl').'http://cdn3.wpbeginner.com/favicon.ico" />';
-4
+function new_excerpt_length($length) {
+return 100;
 }
-5
-add_action('wp_head', 'blog_favicon');
+add_filter('excerpt_length', 'new_excerpt_length');

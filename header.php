1eb22a537bce0eedc4292afdddf9d4148df3a036
Erin <hoople.erin@gmail.com>
2014-02-05 16:29:46 -0600

    created header.php
    transferred header info from index.php to header.php

---
+++ header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<script src="http://use.edgefonts.net/josefin-slab.js"></script>
+	<script src="http://use.edgefonts.net/merriweather.js"></script>
+	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" />
+	<title><?php bloginfo('name'); ?></title>
+</head>

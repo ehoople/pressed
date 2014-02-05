8ca4c981585e96e029b806cc66b33a933d466a19
Erin <hoople.erin@gmail.com>
2014-02-05 16:58:33 -0600

    added body class declaration

--- header.php
+++ header.php
@@ -4,10 +4,11 @@
 	<script src="http://use.edgefonts.net/josefin-slab.js"></script>
 	<script src="http://use.edgefonts.net/merriweather.js"></script>
 	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" />
+	<?php wp_head(); ?>
 	<title><?php bloginfo('name'); ?></title>
 </head>
 
-<body>
+<body <?php body_class( $class ); ?> >
 	<header>
 		<h1> <?php bloginfo('name');?> </h1>
 		<nav>
